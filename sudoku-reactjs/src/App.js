7e007c1a48c506e1fde5669b112d14864de47f6c
Nico Salinas <salinas.nsalinas@gmail.com>
2021-05-26 16:51:36 -0700

    added event bus for clicking cells

--- sudoku-reactjs/src/App.js
+++ sudoku-reactjs/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Board from './components/Board';
 import DifficultySelector from './components/DifficultySelector';
 import React, { Component } from 'react';
+import nanobus from 'nanobus';
+
+export const NanoBus = nanobus();
 
 class App extends Component {
   constructor(props) {
--- sudoku-reactjs/src/components/Cell.js
+++ sudoku-reactjs/src/components/Cell.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
 import "./Cell.css";
+import { NanoBus } from '../App.js';
 
 class Cell extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isEditable: false,
+      highlightCell: false,
     }
   }
 
+  componentDidMount(){
+    NanoBus.on("cell-click", (data) => {
+      if( this.props.value > 0 && this.props.value === data.value) {
+        this.setState({ highlightCell: true });
+      } else if( this.state.highlightCell ) {
+        this.setState({ highlightCell: false });
+      }
+    });
+  }
+
   handleCellClick = () => {
+
+    NanoBus.emit("cell-click", {
+      value: this.props.value
+    });
+
     if( this.props.value === 0 ){
       this.setState({ isEditable: true });
     }
@@ -22,8 +39,13 @@ class Cell extends Component {
   }
 
   render() {
+
+    const cellClassnames = "Cell-container " + 
+      (this.props.locked ? "Cell-container-locked " : "") + 
+      (this.state.highlightCell ? "Cell-container-highlight " : "");
+
     return (
-      <div className={"Cell-container " + (this.props.locked ? "Cell-container-locked" : "")} onClick={this.handleCellClick}>
+      <div className={cellClassnames} onClick={this.handleCellClick}>
         {this.state.isEditable
           ? <input 
               autoFocus
